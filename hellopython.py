7bfb8c3c1fdb5fdfe217a2cf4d4b39934c151540
MTayyab792 <mtayyab792@gmail.com>
2025-03-20 21:22:22 -0700

    second code

--- hellopython.py
+++ hellopython.py
@@ -1,8 +1,7 @@
 ls = ["karachi","Lahore", "Islamabad", "faisalabad", "Multan"]
+c=[]
 for i in ls:   
+    c.insert(0,i)
+    print(c)
     j=i[0].upper() + i[1:-1].lower() + i[-1].upper()
-    print(j)
-    
-    
-
-    
+print(j)
